ec165c620198774f8ccf0bb0dfd21e241384b3b0
Alex <cigan@ua.fm>
2021-01-04 18:16:33 +0200

    Use mapActions and mag Getters to make the code shorter

--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -1,20 +1,28 @@
 <template>
-  <h2>{{ count }}</h2>
-  <button @click="addONe">Addeater</button>
+  <!-- <h2>{{ count }}</h2> -->
+  <h2>{{ finalCounter }}</h2>
+  <!-- <button @click="addONe">Addeater</button> -->
+  <button @click="increaser">Addeater</button>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import { mapActions } from 'vuex';
+
 export default {
   computed: {
-    count() {
-      return this.$store.getters.finalCounter;
-    }
+    // using mapGetters
+    // count() {
+    //   return this.$store.getters.finalCounter;
+    // }
+    ...mapGetters(['finalCounter']) // mapGetters return all getters from state
   },
   methods: {
-    addONe() {
-      //   this.$store.commit('increaser');
-      this.$store.dispatch('increaser');
-    }
+    // addONe() {
+    //   //   this.$store.commit('increaser');
+    //   this.$store.dispatch('increaser');
+    // }
+    ...mapActions(['increaser'])
   }
 };
 </script>
